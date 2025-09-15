ee971e847cfa68f1f3496e7b59bcf921b0e61676
kauagabriel29312-create <kauagabriel29312@gmail.com>
2025-09-15 02:31:53 -0300

    Permitir entrada do nome do usuário para saudação

--- ola_devops.py
+++ ola_devops.py
@@ -1,4 +1,6 @@
 def mensagem(nome):
     print(f"Olá, {nome}! Bem-vindo ao DevOps.")
 
-mensagem("Desenvolvedor")
+if __name__ == "__main__":
+    nome_usuario = input("Digite seu nome: ")
+    mensagem(nome_usuario)
