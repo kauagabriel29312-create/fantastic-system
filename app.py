bde346c251984d40d83627b7e19378bf5afc5213
kauagabriel29312-create <kauagabriel29312@gmail.com>
2025-09-15 11:19:42 -0300

    Recria pasta workflows e adiciona arquivos ci.yml e cd.yml

---
+++ app.py
@@ -0,0 +1,6 @@
+def saudacao(kaua):
+    return f"Olá, {kaua}! Bem-vindo ao mundo DevOps com Python."
+
+if __name__ == "__main__":
+    usuario = input("Digite seu nome: ")
+    print(saudacao(usuario))
---
+++ tests/test_ola.py
@@ -0,0 +1,22 @@
+import subprocess
+
+def test_mensagem_com_nome():
+    # Simula o usuário digitando "Kauã"
+    resultado = subprocess.run(
+        ["python", "ola_devops.py"],
+        input="Kauã\n",       # envia o texto "Kauã" como se fosse digitado
+        capture_output=True,
+        text=True
+    )
+    assert "Olá, Kauã! Bem-vindo ao DevOps." in resultado.stdout
+
+
+def test_mensagem_sem_nome():
+    # Simula o usuário apenas apertando Enter (nome vazio)
+    resultado = subprocess.run(
+        ["python", "ola_devops.py"],
+        input="\n",           # apenas Enter
+        capture_output=True,
+        text=True
+    )
+    assert "Olá, Visitante! Bem-vindo ao DevOps." in resultado.stdout
